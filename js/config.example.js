fbbb0cd34321a172ad4d1ccf2b2d33f3dc8c837c
mlynpa <mlynpa@gmail.com>
2026-09-01 11:48:38 +0200

    Serve basemap key as a plain static file for reliable Pages deploy

--- js/config.example.js
+++
@@ -1,4 +0,0 @@
-// Copy this file to js/config.js and fill in your own key.
-// Get a free CARTO basemaps API key (no account needed, ~1 min):
-// https://carto.com/basemaps/apikey
-window.CARTO_API_KEY = "YOUR_CARTO_API_KEY";
---
+++ js/config.js
@@ -0,0 +1,1 @@
+window.CARTO_API_KEY = "cb1_2p2o_1_2e36f6774d0becb32eb6e877";
