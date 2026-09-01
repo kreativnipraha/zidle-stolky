82cb2822c6883b65bbad056cc6fd123f0bd9dd2d
mlynpa <mlynpa@gmail.com>
2026-09-01 11:41:50 +0200

    Initial site setup

---
+++ js/config.example.js
@@ -0,0 +1,4 @@
+// Copy this file to js/config.js and fill in your own key.
+// Get a free CARTO basemaps API key (no account needed, ~1 min):
+// https://carto.com/basemaps/apikey
+window.CARTO_API_KEY = "YOUR_CARTO_API_KEY";
